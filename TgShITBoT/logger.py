b12287335a0693fc331f04e9f608687705b86ee8
mhmd <virous.dev@gmail.com>
2026-07-18 12:24:21 +0300

    Update logger.py

--- TgShITBoT/logger.py
+++ TgShITBoT/logger.py
@@ -1,12 +1,13 @@
+import os
 import logging
 
+LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO").upper()
+
 logging.basicConfig(
-    level=logging.INFO,
+    level=getattr(logging, LOG_LEVEL, logging.INFO),
     format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
     datefmt="%d-%b-%y %H:%M:%S",
 )
-
-# logging.getLogger("pyrogram").setLevel(logging.CRITICAL)
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
